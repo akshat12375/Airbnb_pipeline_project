f3691a433c83f1454696a6a722e6bbbc4c672b7d
Akshat Srivastava <84175297+akshat12375@users.noreply.github.com>
2026-06-21 21:59:38 +0530

    changed the source name in the bronze models

--- pipeline_project_dbt/models/bronze/bronze_listings.sql
+++ pipeline_project_dbt/models/bronze/bronze_listings.sql
@@ -1,8 +1,10 @@
 {{ config(materialized='incremental') }}
 
 
-SELECT * FROM  {{ source('staging', 'listings') }}
+SELECT * FROM  {{ source('source_stage', 'listings') }}
 
 {% if is_incremental() %}
     WHERE CREATED_AT > (SELECT COALESCE(MAX(CREATED_AT), '1900-01-01') FROM {{ this }})
+    -- I could have used the ref but 'this' is more efficient
+    -- using coalesce to handle the case when the table is empty and there is no max created_at
 {% endif %}
