dafa73fbaf32331fc34eb60bbd478e714982df54
Akshat Srivastava <84175297+akshat12375@users.noreply.github.com>
2026-06-21 21:50:41 +0530

    applied incremental loading in the bronze layer

--- pipeline_project_dbt/models/bronze/bronze_hosts.sql
+++ pipeline_project_dbt/models/bronze/bronze_hosts.sql
@@ -1,2 +1,8 @@
-select * from 
-{{source('source_stage', 'hosts')}}
+{{ config(materialized='incremental') }}
+
+
+SELECT * FROM  {{ source('staging', 'hosts') }}
+
+{% if is_incremental() %}
+    WHERE CREATED_AT > (SELECT COALESCE(MAX(CREATED_AT), '1900-01-01') FROM {{ this }})
+{% endif %}
